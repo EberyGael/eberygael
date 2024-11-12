68046eed535aff3674da9bc8c79bbdc9eaf9c155
unknown <eberesquivel@gmail.com>
2024-11-11 20:21:04 -0600

    .

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -13,18 +11,3 @@ def renderPosts(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, "post_detail.html", {"post": post})
-
-def form_view(request):
-    return render(request, 'form.html')
-
-def enviar_mensaje(request):
-    if request.method == 'POST':
-        mensaje = request.POST.get('mensaje')
-        
-        # Procesa el mensaje o envíalo a algún lugar
-        print("Mensaje recibido:", mensaje)  # Esto se verá en la consola
-        
-        # Redirige a la página de éxito o al formulario
-        return redirect(reverse('form'))
-    else:
-        return HttpResponse("Método no permitido", status=405)
